a2edc498a16acfa49b384ab1f02bb6a2f76c8307
TuMercado <tumercado17@gmail.com>
2017-06-05 20:13:24 -0300

    se actuliza conexion al servidor

--- logica/funciones.php
+++ logica/funciones.php
@@ -1,13 +1,9 @@
 <?php
-require_once('config.php');
 
-
-function conectar()
-{
+function conectar(){
     try {
-        $conexion = new PDO('mysql:host=localhost;dbname=7curso', 'root', '');
-        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+        $conexion = new PDO('mysql:host=localhost;dbname=base.sitio', 'root', '');
+        $conexion->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
         return($conexion);
     } catch (PDOException $e) {
 
@@ -17,19 +13,55 @@ function conectar()
     }
 }
 
-
 function desconectar($conexion)
 {
    $conexion=null;
 
 }
 
-function salir() {
-	session_unset();
-	session_destroy();
 
-}
+  function sessionStart(){
+    session_start();
+    $_SESSION['state'] = 'std';
+    $_SESSION['logged'] = false;
 
+  }
+
+
+  function sessionCheck(){
+
+  		 if(!isset($_SESSION)){
+            session_start();
+
+          if(isset($_SESSION['state']) and !empty($_SESSION['state'])){										//SE REVISA QUE LA VARIABLE DE ESTADO DE LA SESION ESTÉ SETEADA
+
+      		 }
+      		 else{
+        			$_SESSION['state'] = 'cld';
+        			$_SESSION['logged'] = false;
+
+        			if (file_exists("./index.php")){
+        				Require_once("./index.php");
+                ?>
+                <script> window.alert("Sesión expirada, inicie sesión nuevamente."); </script>
+                <?php
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=../presentacion/index.php\" />";//REDIRIGE AL USUARIO AL INICIO
+        			}
+        			elseif(file_exists("../presentacion/index.php")){
+        				Require_once("../presentacion/index.php");
+                ?>
+                <script> window.alert("Sesión expirada, inicie sesión nuevamente."); </script>
+                <?php
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=../presentacion/index.php\" />";//REDIRIGE AL USUARIO AL INICIO
+        			}
+      		 }
+        }
+  }
+
+  function sessionClose(){
+		session_unset();
+		session_destroy();
+	}
 
 
 
