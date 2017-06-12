89f97457193822f9dc8ac4a1488786eaecd9fdce
TuMercado <tumercado17@gmail.com>
2017-06-12 20:06:00 -0300

    actuliza funcion

--- frontend/logica/funciones.php
+++ frontend/logica/funciones.php
@@ -2,7 +2,7 @@
 
 function conectar(){
     try {
-        $conexion = new PDO('mysql:host=162.216.19.206;port=443;dbname=proyecto_fassani','fassani','VnalkNMlFOoxHlkIYkXS');
+        $conexion = new PDO('mysql:host=localhost;dbname=base.sitio', 'root', '');
         $conexion->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
         return($conexion);
     } catch (PDOException $e) {
@@ -13,14 +13,55 @@ function conectar(){
     }
 }
 
-function desconectar($conexion){
+function desconectar($conexion)
+{
    $conexion=null;
+
 }
 
-function salir(){
-	session_unset();
-	session_destroy();
-}
+
+  function sessionStart(){
+    session_start();
+    $_SESSION['state'] = 'std';
+    $_SESSION['logged'] = false;
+    $_SESSION['path'] = getcwd();
+
+  }
+
+
+  function sessionCheck(){
+
+  		 if(!isset($_SESSION)){
+            session_start();
+          if(isset($_SESSION['state']) and !empty($_SESSION['state'])){										//SE REVISA QUE LA VARIABLE DE ESTADO DE LA SESION ESTÉ SETEADA
+
+      		 }
+      		 else{
+        			$_SESSION['state'] = 'cld';
+        			$_SESSION['logged'] = false;
+
+        			if (file_exists("./mainMenu.php")){
+        				Require_once("./mainMenu.php");
+                ?>
+                <script> window.alert("Sesión expirada, inicie sesión nuevamente."); </script>
+                <?php
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=../presentacion/mainMenu.php\" />";//REDIRIGE AL USUARIO AL INICIO
+        			}
+        			elseif(file_exists("../presentacion/mainMenu.php")){
+        				Require_once("../presentacion/mainMenu.php");
+                ?>
+                <script> window.alert("Sesión expirada, inicie sesión nuevamente."); </script>
+                <?php
+                echo "<meta http-equiv=\"refresh\" content=\"0; url=../presentacion/mainMenu.php\" />";//REDIRIGE AL USUARIO AL INICIO
+        			}
+      		 }
+        }
+  }
+
+  function sessionClose(){
+		session_unset();
+		session_destroy();
+	}
 
 
 
